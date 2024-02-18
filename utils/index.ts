7f771c746af78ab4bb4a1e70c85d7def5154b6f5
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2024-02-18 23:21:54 +0100

    fix: made static methods and properties readonly

--- utils/index.ts
+++ utils/index.ts
@@ -1,70 +1,72 @@
+import { Validator } from "../types";
+
 export class RegexPatterns {
-    static ALPHANUMERIC = /^([a-zA-Z0-9]+)$/;
-    static EMAIL = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-    static NUMERIC = /^([0-9]+)$/;
-    static PHONE_NUMBER = /^\d{3}-\d{3}-\d{4}$/;
-    static DATE = /^\d{4}-\d{2}-\d{2}$/;
-    static URL = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?$/;
-    static UPPERCASE = /^[A-Z]+$/;
-    static LOWERCASE = /^[a-z]+$/;
-    static HEX_COLOR = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-    static CREDIT_CARD_NUMBER = /^\d{4}-?\d{4}-?\d{4}-?\d{4}$/;
+    static readonly ALPHANUMERIC = /^([a-zA-Z0-9]+)$/;
+    static readonly EMAIL = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    static readonly NUMERIC = /^([0-9]+)$/;
+    static readonly PHONE_NUMBER = /^\d{3}-\d{3}-\d{4}$/;
+    static readonly DATE = /^\d{4}-\d{2}-\d{2}$/;
+    static readonly URL = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- .\\/?%&=]*)?$/;
+    static readonly UPPERCASE = /^[A-Z]+$/;
+    static readonly LOWERCASE = /^[a-z]+$/;
+    static readonly HEX_COLOR = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    static readonly CREDIT_CARD_NUMBER = /^\d{4}-?\d{4}-?\d{4}-?\d{4}$/;
 }
   
 export class ValidationRules {
-    static ALPHANUMERIC = {
+    public static readonly ALPHANUMERIC = {
       name: "ALPHANUMERIC",
       pattern: RegexPatterns.ALPHANUMERIC,
     };
   
-    static EMAIL = {
+    static readonly EMAIL = {
       name: "EMAIL",
       pattern: RegexPatterns.EMAIL,
     };
   
-    static NUMERIC = {
+    static readonly NUMERIC = {
       name: "NUMERIC",
       pattern: RegexPatterns.NUMERIC,
     };
   
-    static PHONE_NUMBER = {
+    static readonly PHONE_NUMBER = {
       name: "PHONE_NUMBER",
       pattern: RegexPatterns.PHONE_NUMBER,
     };
   
-    static DATE = {
+    static readonly DATE = {
       name: "DATE",
       pattern: RegexPatterns.DATE,
     };
   
-    static URL = {
+    static readonly URL = {
       name: "URL",
       pattern: RegexPatterns.URL,
     };
   
-    static UPPERCASE = {
+    static readonly UPPERCASE = {
       name: "UPPERCASE",
       pattern: RegexPatterns.UPPERCASE,
     };
   
-    static LOWERCASE = {
+    static readonly LOWERCASE = {
       name: "LOWERCASE",
       pattern: RegexPatterns.LOWERCASE,
     };
   
-    static HEX_COLOR = {
+    static readonly HEX_COLOR = {
       name: "HEX_COLOR",
       pattern: RegexPatterns.HEX_COLOR,
     };
   
-    static CREDIT_CARD_NUMBER = {
+    static readonly CREDIT_CARD_NUMBER = {
       name: "CREDIT_CARD_NUMBER",
       pattern: RegexPatterns.CREDIT_CARD_NUMBER,
     };
 }
   
 export class ValidatorFactory {
-    static createValidator(name: PatternName, pattern: string | RegExp): Validator {
+    static createValidator(name: Validator['name'], pattern: string | RegExp): Validator {
       return {
         name,
         pattern,
