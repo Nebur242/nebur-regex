b7b1081fe1efbcf24cdd9b4cfb39f67d5a803881
Nebur242 <57473126+Nebur242@users.noreply.github.com>
2026-07-18 12:37:22 -0400

    Optimize package output for ESM and CommonJS (#9)
    * build: optimize package output
    
    * fix: make CI installs reproducible
    
    * fix: build before measuring package size

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
+  watchman: false,
   collectCoverageFrom: ["src/**/*.ts", "!src/tests/**"],
   coverageThreshold: {
     global: {
